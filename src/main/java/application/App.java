2007f3f4ac3670b186b43be18580c2ea5cd8f42d
JayKL <Jaykonrad-lee@hotmail.com>
2019-05-21 14:39:22 +0100

    added JSON string creator

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -4,9 +4,16 @@ import AccountManagement.AccountManagement;
 import ServiceForApp.Service;
 
 public class App{
+	public static void main(String xyz[]) {
+		App appref = new App();
+	}
+	
 	public App() {
-		AccountManagement createAccountsRefVar=new AccountManagement();
+		AccountManagement accountManageVar=new AccountManagement();
 		Service serviceRefVar = new Service();
+		accountManageVar.createAccount("Gnome", "King");
+		accountManageVar.createAccount("Ben", "Till");
 		System.out.println("Hello world to the standard out");
+		accountManageVar.createJSONFromMap();
 	}
 }
